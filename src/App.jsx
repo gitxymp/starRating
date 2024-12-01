1adbe296f7a043524c0c4462048ac8c52f6b8cef
gitxymp <147992942+gitxymp@users.noreply.github.com>
2024-12-02 01:23:20 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import StarRating from "./components/star-rating";
+
+function App() {
+  return (
+    <div className="App">
+      <StarRating noOfStars={10} />
+    </div>
+  );
+}
+
+export default App;
